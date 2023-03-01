9e0e3eef87048f07a5d7ccd80680653e593441b6
bfabien99 <fabienbrou99@gmail.com>
2023-03-01 09:47:02 +0000

    little update

--- revstr.php
+++ revstr.php
@@ -1,12 +1,8 @@
 <?php
 // Écrivez un programme PHP pour inverser une chaîne sans utiliser la fonction de chaîne intégrée
-$chaine = "awas";
+$chaine = "Stamp";
 $rev = "";
 for($i=1;$i<=strlen($chaine);$i++){
-    if($i == strlen($chaine)){
-        $rev.= $chaine[0];
-        break;
-    }
     $rev.= $chaine[-$i];
 }
 echo $rev;
